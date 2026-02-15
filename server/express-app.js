63eeaaaa61918298e19c8699294867265a4043d8
Husen Nh <hd.website19@gmail.com>
2026-02-15 23:36:47 +0700

    Update express-app.js

--- server/express-app.js
+++ server/express-app.js
@@ -12,6 +12,10 @@ app.use(express.json({ limit: '2mb' }));
 app.post('/api/data', (req, res) => {
     handleGetData(req, res, req.body || {});
 });
+app.get('/api/data', (req, res) => {
+    const action = req.query.action || 'get_public_data';
+    handleGetData(req, res, { action });
+});
 app.post('/api/update', (req, res) => {
     handleUpdateData(req, res, req.body || {});
 });
